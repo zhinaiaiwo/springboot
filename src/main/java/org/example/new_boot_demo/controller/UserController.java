b8fe694905035eaee958c282445c1934fd0765a6
zhinaiaiwo <192845124@qq.com>
2026-05-01 00:44:13 +0800

    2026-4-29

--- src/main/java/org/example/new_boot_demo/controller/UserController.java
+++ src/main/java/org/example/new_boot_demo/controller/UserController.java
@@ -89,7 +89,7 @@ public class UserController {
 
 
     @PutMapping("/update")
-    public Result update(@RequestBody User user) {
+    public Result update(@RequestBody @Validated User user) {
         userService.update(user);
         return Result.success();
     }
--- src/main/java/org/example/new_boot_demo/pojo/User.java
+++ src/main/java/org/example/new_boot_demo/pojo/User.java
@@ -1,17 +1,29 @@
 package org.example.new_boot_demo.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class User {
+    @NotNull
     private Integer id;//主键ID
     private String username;//用户名
+
     @JsonIgnore // 让 springmvc 把当前对象转换成 JSON 字符串时忽略 password， 最终的 JSON 字符串中就会没有 password 属性了
     private String password;//密码
+
+    @NotEmpty
+    @Pattern(regexp = "^\\s{1,10}$")
     private String nickname;//昵称
+
+    @NotEmpty
+    @Email
     private String email;//邮箱
     private String userPic;//用户头像地址
     private LocalDateTime createTime;//创建时间
